819c2870a959138e39e6e89df4e3b91f68a22701
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2021-11-17 09:35:36 +0100

    Add project files.

---
+++ TSO_CSHARP_THEMA_4_GUI_MET_WPF_BASIS/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TSO_CSHARP_THEMA_4_GUI_MET_WPF_BASIS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ TSO_CSHARP_THEMA_4_GUI_MET_WPF_BASIS/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TSO_CSHARP_THEMA_4_GUI_MET_WPF_BASIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
